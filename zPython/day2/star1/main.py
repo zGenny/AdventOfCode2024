596b480440724fc456ad69468186ed9121d0aacb
zGenny <gennarodegregorio03@gmail.com>
2024-12-02 16:04:39 +0100

    completed day2-star1

---
+++ zPython/day2/star1/main.py
@@ -0,0 +1,30 @@
+
+#! Ogni riga o incrementa o decresce
+#! Può essere incrementata o decrementata di 1, 2 o 3
+#! Non può rimanere costante
+
+def safe(int_list):
+  if int_list[0] < int_list[1]:
+    for i in range(len(int_list)-1):
+      if int_list[i] < int_list[i+1] and abs(int_list[i+1] - int_list[i]) <= 3:
+        continue
+      else:
+        return 0
+    return 1
+  elif int_list[0] > int_list[1]:
+    for i in range(len(int_list)-1):
+      if int_list[i] > int_list[i+1] and abs(int_list[i+1] - int_list[i]) <= 3:
+        continue
+      else:
+        return 0
+    return 1
+  else:
+    return 0
+
+tot = 0
+
+with open("input.txt", "r") as file:
+  for line in file:
+    tot += safe(list(map(int, line.split())))
+
+print(tot)
